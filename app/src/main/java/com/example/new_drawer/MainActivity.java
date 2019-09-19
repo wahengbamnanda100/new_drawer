66f11621c9113db6df583420c04641d11432d3c2
NandaKumar <nandakumar@NandaKumars-MacBook-Air.local>
2019-09-19 06:03:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/new_drawer/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.new_drawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new home()).commit();
+
+                        break;
+
+                    case R.id.nav_order:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new order()).commit();
+
+                        break;
+
+                    case R.id.nav_cart:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new cart()).commit();
+
+                        break;
+
+                    case R.id.nav_notifications:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new notification()).commit();
+
+                        break;
+
+                    case R.id.nav_share:
+                        Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_contact:
+                        Toast.makeText(MainActivity.this, "Contact", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomListner);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new home()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomListner = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()) {
+                case R.id.bot_home :
+                    selectedFragment = new home();
+                    break;
+
+                case R.id.bot_order :
+                    selectedFragment = new order();
+                    break;
+
+                case R.id.bot_cart :
+                    selectedFragment = new cart();
+                    break;
+
+                case R.id.bot_notification :
+                    selectedFragment = new notification();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+            return true; 
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
